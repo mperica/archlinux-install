047b8f151a6ae9e26f7747a81482b66b0b6b4804
Martin Perica <martinperica0@gmail.com>
2020-01-03 13:23:04 +0100

    update menus

--- install.sh
+++ install.sh
@@ -1,62 +1,22 @@
 #!/usr/bin/env sh
 
 
-APP="dialog"
-BACKTITLE="TEST Install"
+app="dialog"
+title="ArchLinux Install Script"
+menu_main="Main Menu"
+menu_disk="Disk Management"
+menu_install="Install Menu"
+select_exit="Exit"
+select_done="Done"
+select_disk="Select Disk"
+select_disk_part="Create Partitions"
+select_disk_format="Format Partitions"
+select_disk_mount="Mount Disk"
+select_editor="Select Editor"
+select_install_base="Install Base System"
 
 
-SELECT_DISK="Select Disk"
-SELECT_DISKPART="Create Partitions"
-SELECT_DISKFORMAT="Format Partitions"
-SELECT_DISKMOUNT="Mount Disk"
-SELECT_EDITOR="Select Editor"
-SELECT_MAIN_MENU="Main Menu"
-SELECT_DISK_MENU="Disk Management"
-SELECT_EXIT="Exit"
-SELECT_DONE="Done"
-
-pressanykey(){
-  read -n1 -p "Press any key to continue..."
-}
-
-mainmenu(){
-  if [ "${1}" == "" ];then
-    nextitem="."
-  else
-    nextitem="${1}"
-  fi
-
-  options=()
-  options+=("${SELECT_EDITOR}" "")
-  options+=("${SELECT_DISK_MENU}" "")
-  options+=("${SELECT_EXIT}" "")
-
-  select=`"${APP}" \
-	  --backtitle "${BACKTITLE}" \
-	  --title "${SELECT_MAIN_MENU}" \
-	  --default-item "${nextitem}" \
-		--no-cancel \
-	  --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3`
-
-  if [ "$?" == "0" ];then
-    case ${select} in
-      "${SELECT_EDITOR}")
-        selecteditor
-				nextitem="${SELECT_DISK_MENU}"
-      ;;
-      "${SELECT_DISK_MENU}")
-        diskmenu
-				nextitem="${SELECT_EXIT}"
-      ;;
-      "${SELECT_EXIT}")
-        ${APP} --defaultno --yesno "Are you sure you whant to exit?" 0 0 && clear && exit 0
-      ;;
-    esac
-    mainmenu "${nextitem}"
-  else
-    clear
-  fi
-}
+## Functions ##
 
 pressanykey(){
   read -n1 -p "Press any key to continue..."
@@ -67,15 +27,15 @@ selecteditor(){
   options+=("vim" "")
   options+=("nano" "")
 
-  select=`"${APP}" \
-	  --backtitle "${BACKTITLE}" \
-	  --title "${SELECT_EDITOR}" \
+  select=`"${app_name}" \
+	  --backtitle "title" \
+	  --title "${select_editor}" \
 	  --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3`
   if [ "$?" = "0" ];then
     echo "Selected editor is ${select}"
     export EDITOR=${select}
     EDITOR=${select}
-    ${APP} --msgbox "Selected Editor is ${select}" 5 30
+    ${app_name} --msgbox "Selected Editor is ${select}" 5 30
   fi
 }
 
@@ -89,66 +49,59 @@ selectdisk(){
   done
   IFS=$IFS_ORIG
 
-  result=`"${APP}" --backtitle "${BACKTITLE}" --title "${SELECT_DISK}" --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3`
+  result=`"${app_name}" --backtitle "title" --title "${select_disk}" \
+      --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3`
   if [ "$?" != "0" ];then
     return 1
   fi
-  #clear
-	#dialog --msgbox "Selected Disk is \n${result}" 10 20
 	echo ${result}
 	disk=$(echo ${result} | awk '{print $1}')
-	export INSTALL_DISK="${disk}"
+	export install_disk="${disk}"
   return 0
 }
 
 partdisk(){
-	#INSTALL_DISK=$(selectdisk)
 	selectdisk
-	${APP} --backtitle "${BACKTITLE}" --title "${SELECT_DISKPART} (gpt)" \
-			--defaultno --yesno "Selected device : ${INSTALL_DISK}\n\nAll data will be erased ! \n\nContinue ?" 0 0
+	${app_name} --backtitle "title" --title "${select_disk_part}" \
+			--defaultno --yesno "Selected device : ${install_disk}\n\nAll data will be erased ! \n\nContinue ?" 0 0
 	if [ "$?" = "0" ];then
 	  clear
-	  echo "Creating a new gpt table on ${INSTALL_DISK}"
-	  parted -s ${INSTALL_DISK} mklabel gpt
-	  echo "Creating boot EFI partition on ${INSTALL_DISK}"
-	  parted -s ${INSTALL_DISK} mkpart ESP fat32 1M 512M
-	  parted -s ${INSTALL_DISK} set 1 boot on
-	  #sgdisk ${INSTALL_DISK} -n=1:0:+512M -t=1:ef00
-	  #SWAP=$(cat /proc/meminfo | grep MemTotal | awk '{ print $2 }')
-	  #SWAP=$((${SWAP}/1000))"M"
-	  #echo "Creating swap partition on ${INSTALL_DISK} with size of ${SWAP}"
-	  #sgdisk ${INSTALL_DISK} -n=2:0:+${SWAP} -t=3:8200
-	  echo "Creating root partition on ${INSTALL_DISK}"
-	  #sgdisk ${INSTALL_DISK} -n=2:0:0
-	  parted -s ${INSTALL_DISK} mkpart btrfs 513M 100%
+	  echo "Creating a new gpt table on ${install_disk}"
+	  parted -s ${install_disk} mklabel gpt
+	  echo "Creating boot EFI partition on ${install_disk}"
+	  parted -s ${install_disk} mkpart ESP fat32 1M 512M
+	  parted -s ${install_disk} set 1 boot on
+	  parted -s ${install_disk} name 1 BOOT
+	  echo "Creating root partition on ${install_disk}"
+	  parted -s ${install_disk} mkpart btrfs 513M 100%
+	  parted -s ${install_disk} name 2 ROOT
 	  pressanykey
 	  clear
-	  echo "CRYPT SETUP"
-          cryptdisk
+    echo
+	  echo "CRYPT SETUP\n"
+    echo
+    cryptdisk
 	else
-	  ${APP} --msgbox "Disk modification canceled" 0 0
+	  ${app_name} --msgbox "Disk modification canceled" 0 0
 	fi
 }
 
 cryptdisk(){
-	# Create luks container (luks1 for compatibility with grub)
-	echo "Creating LUKS Disk"
 	cryptsetup -q --type luks1 --cipher aes-xts-plain64 --hash sha512 \
-	    --use-random --verify-passphrase luksFormat ${INSTALL_DISK}2
-	# Create btrfs filesystem
-	echo "Unlocking LUKS Disk"
-	cryptsetup open ${INSTALL_DISK}2 archlinux
+	    --use-random --verify-passphrase luksFormat ${install_disk}2
+	cryptsetup open ${install_disk}2 archlinux
 }
 
 formatdisk(){
-	${APP} --backtitle "${BACKTITLE}" --title "${SELECT_DISKFORMAT} (btrfs)" \
-	    --defaultno --yesno "Formating disk: ${INSTALL_DISK}\n\n
-	    ${INSTALL_DISK}1 fat32\n
+	${app_name} --backtitle "title" --title "${select_disk_format} (btrfs)" \
+	    --defaultno --yesno "Formating disk: ${install_disk}\n\n
+	    ${install_disk}1 fat32\n
 	    /dev/mapper/archlinux btrfs\n
 	    \n\nContinue ?" 0 0
+
 	if [ "$?" = "0" ];then
 	    clear
-	    mkfs.vfat -F32 ${INSTALL_DISK}1
+	    mkfs.vfat -F32 ${install_disk}1
 	    mkfs -t btrfs --force -L archlinux /dev/mapper/archlinux
 	    mount /dev/mapper/archlinux /mnt
 	    btrfs subvolume create /mnt/@
@@ -159,7 +112,7 @@ formatdisk(){
 	    umount -R /mnt
 	    pressanykey
 	else
-	    ${APP} --msgbox "Disk formating canceled" 0 0
+	    ${app_name} --msgbox "Disk formating canceled" 0 0
 	fi
 }
 
@@ -173,12 +126,68 @@ mountdisk(){
 	mount -o compress=lzo,subvol=@cache,$o_btrfs /dev/mapper/archlinux /mnt/var/cache
 	mount -o compress=lzo,subvol=@snapshots,$o_btrfs /dev/mapper/archlinux /mnt/.snapshots
 	mkdir -p /mnt/boot
-	mount ${INSTALL_DISK}1 /mnt/boot
+	mount ${install_disk}1 /mnt/boot
 	pressanykey
 }
 
+# =============INSTALL===============#
+installbase(){
+# Install the system, wifi and some tools
+  pacstrap /mnt base base-devel btrfs-progs snapper zsh htop \
+    net-tools wireless_tools wpa_supplicant dialog vim \
+    intel-ucode grub aria2 curl bash-completion
 
-# MENUS
+	# Generate fstab
+	genfstab -L -p /mnt >> /mnt/etc/fstab
+}
+
+### MENUS ###
+
+mainmenu(){
+  if [ "${1}" == "" ];then
+    nextitem="."
+  else
+    nextitem="${1}"
+  fi
+
+  options=()
+  options+=("${select_editor}" "")
+  options+=("${menu_disk}" "")
+  options+=("${menu_install}" "")
+  options+=("${select_exit}" "")
+
+  select=`"${app_name}" \
+	  --backtitle "title" \
+	  --title "${menu_main}" \
+	  --default-item "${nextitem}" \
+		--no-cancel \
+	  --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3`
+
+  if [ "$?" == "0" ];then
+    case ${select} in
+      "${select_editor}")
+        selecteditor
+				nextitem="${menu_disk}"
+      ;;
+      "${menu_disk}")
+        diskmenu
+				nextitem="${menu_install}"
+      ;;
+      "${menu_install}")
+        installmenu
+				nextitem="${select_exit}"
+      ;;
+      "${select_exit}")
+        ${app_name} --defaultno --yesno "Are you sure you whant to exit?" 0 0 && clear && exit 0
+      ;;
+    esac
+    mainmenu "${nextitem}"
+  else
+    clear
+  fi
+}
+
+
 diskmenu(){
   if [ "${1}" == "" ];then
     nextitem="."
@@ -187,33 +196,32 @@ diskmenu(){
   fi
 
   options=()
-  options+=("${SELECT_DISKPART}" "")
-  options+=("${SELECT_DISKFORMAT}" "")
-  options+=("${SELECT_DISKMOUNT}" "")
-  options+=("${SELECT_DONE}" "")
-}
+  options+=("${select_disk_part}" "")
+  options+=("${select_disk_format}" "")
+  options+=("${select_disk_mount}" "")
+  options+=("${select_done}" "")
 
-  select=`"${APP}" \
-	  --backtitle "${BACKTITLE}" \
-	  --title "${SELECT_DISK_MENU}" \
+  select=`"${app_name}" \
+	  --backtitle "title" \
+	  --title "${menu_disk}" \
 	  --default-item "${nextitem}" \
 	  --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3`
 
   if [ "$?" == "0" ];then
     case ${select} in
-      "${SELECT_DISKPART}")
+      "${select_disk_part}")
         partdisk
-	nextitem="${SELECT_DISKFORMAT}"
+	nextitem="${select_disk_format}"
       ;;
-      "${SELECT_DISKFORMAT}")
+      "${select_disk_format}")
         formatdisk
-	nextitem="${SELECT_DISKMOUNT}"
+	nextitem="${select_disk_mount}"
       ;;
-      "${SELECT_DISKMOUNT}")
+      "${select_disk_mount}")
         mountdisk
-	nextitem="${SELECT_DONE}"
+	nextitem="${select_done}"
       ;;
-      "${SELECT_DONE}")
+      "${select_done}")
 	mainmenu
       ;;
     esac
@@ -223,5 +231,39 @@ diskmenu(){
   fi
 }
 
-mainmenu
+installmenu(){
+  if [ "${1}" == "" ];then
+    nextitem="."
+  else
+    nextitem="${1}"
+  fi
 
+  options=()
+  options+=("${menu_install}" "")
+  options+=("${select_done}" "")
+
+  select=`"${app_name}" \
+	  --backtitle "title" \
+	  --title "${menu_install}" \
+	  --default-item "${nextitem}" \
+	  --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3`
+
+  if [ "$?" == "0" ];then
+    case ${select} in
+      "${select_install_base}")
+        installbase
+        nextitem="${select_done}"
+      ;;
+      "${select_done}")
+      mainmenu
+      ;;
+    esac
+    diskmenu "${nextitem}"
+  else
+    clear
+  fi
+}
+
+
+# Start default menu
+mainmenu
